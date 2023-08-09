44323ce1c199726a1a7b8673af23b5d3375b5342
MariaMotovilova <capusmurcus@yandex.ru>
2023-08-09 22:47:47 +0300

    Дорабатывает код

--- js/popup.js
+++ js/popup.js
@@ -5,22 +5,50 @@ const cardTemplate = document.querySelector('#card').content.querySelector('.pop
 
 const advertisements = getAdvertisements();
 
+const offerTypes = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
 
 const renderAdvertisements = () => {
   const advertisementsListFragment = document.createDocumentFragment();
 
   advertisements.forEach(({author, offer}) => {
     const popup = cardTemplate.cloneNode(true);
-    popup.querySelector('.popup__title').textContent = offer.title;
-    popup.querySelector('.popup__text--address').textContent = offer.address;
-    popup.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
-    popup.querySelector('.popup__type').textContent = offer.type; // сопоставить с подписями!! Квартира - flat, Бунгало - bungalow, Дом - house, Дворец - palace, Отель - hotel
-    popup.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-    popup.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-    popup.querySelector('.popup__features').textContent = offer.features;
-    popup.querySelector('.popup__description').textContent = offer.description;
-    popup.querySelector('.popup__photos').setAttribute('src', offer.photos);
-    popup.querySelector('.popup__avatar').setAttribute('src', author.avatar);
+    if (offer.title) {
+      popup.querySelector('.popup__title').textContent = offer.title;
+    }
+    if (offer.address) {
+      popup.querySelector('.popup__text--address').textContent = offer.address;
+    }
+    if (offer.price) {
+      popup.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
+    }
+    if (offer.type) {
+      popup.querySelector('.popup__type').textContent = offerTypes[offer.type];
+    }
+    if (offer.rooms && offer.guests) {
+      popup.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+    }
+    if (offer.checkin && offer.checkout) {
+      popup.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+    }
+    if (offer.features) {
+      popup.querySelector('.popup__features').textContent = offer.features;
+    }
+    if (offer.description) {
+      popup.querySelector('.popup__description').textContent = offer.description;
+    }
+    if (offer.photos) {
+      popup.querySelector('.popup__photos').setAttribute('src', offer.photos);
+    }
+    if (author.avatar) {
+      popup.querySelector('.popup__avatar').setAttribute('src', author.avatar);
+    }
+
     advertisementsListFragment.appendChild(popup);
   });
 
@@ -29,5 +57,4 @@ const renderAdvertisements = () => {
 };
 renderAdvertisements();
 
-// Предусмотрите ситуацию, когда данных для заполнения не хватает. Например, отсутствует описание. В этом случае соответствующий блок в карточке скрывается.
 
